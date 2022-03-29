0afe3bbc0da358828fab9b72a30df2ca89afa00d
Roki_Crazy <89066095+roki200@users.noreply.github.com>
2022-03-29 21:12:41 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ from aiogram.dispatcher import Dispatcher
 from aiogram import Bot
 import os
 
-TOKEN = os.getenv('BOT_TOKEN')
+TOKEN = os.getenv('5278716666:AAHfOamFyjAY1ngishSIAHQ6rVDoQAJoKV0')
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+HEROKU_APP_NAME = os.getenv('echobottgfd')
 
 # webhook settings
 WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
