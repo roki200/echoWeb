980cb5ff76c2cb49c7442d8cd5b14f55f8217451
Roki_Crazy <89066095+roki200@users.noreply.github.com>
2022-04-01 18:57:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,29 @@ async def on_shutdown(dp):
     #await database.disconnect() это бд той статьи
 
 
+async def on_startup(dispatcher):
+    await database.connect()
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+
+
+async def on_shutdown(dispatcher):
+    await database.disconnect()
+    await bot.delete_webhook()
+
+
+async def save(user_id, text):
+    await database.execute(f"INSERT INTO messages(telegram_id, text) "
+                           f"VALUES (:telegram_id, :text)", values={'telegram_id': user_id, 'text': text})
+
+
+async def read(user_id):
+    results = await database.fetch_all('SELECT text '
+                                       'FROM messages '
+                                       'WHERE telegram_id = :telegram_id ',
+                                       values={'telegram_id': user_id})
+    return [next(result.values()) for result in results]
+
+
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
     await message.reply("Привет,напиши мне что нибудь!")
