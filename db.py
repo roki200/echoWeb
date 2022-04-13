144e97cb9b005267a61a81091367a8d363c4b3e9
Roki_Crazy <89066095+roki200@users.noreply.github.com>
2022-04-13 17:45:19 +0700

    Update db.py

--- db.py
+++ db.py
@@ -2,18 +2,5 @@ from databases import Database
 from config import DB_URL
 
 
-CREATE TABLE messages (
-id SERIAL PRIMARY KEY,
-telegram_id INTEGER NOT NULL,
-text text NOT NULL
-);
 
-
-async def read(user_id):
-    results = await database.fetch_all('SELECT text '
-                                       'FROM messages '
-                                       'WHERE telegram_id = :telegram_id ',
-                                       values={'telegram_id': user_id})
-    return [next(result.values()) for result in results]
-  
 database = Database(DB_URL)
